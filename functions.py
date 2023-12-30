a83ebd37636c1d84a6a1493a9e601fddbf53c510
alerch97 <alexlerch76@gmail.com>
2023-12-30 11:23:00 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of to-do items.
+    """
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file.
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Module functions.py is executed.")
+    print(f"Saved to do's in {FILEPATH} area:")
+    print(get_todos())
